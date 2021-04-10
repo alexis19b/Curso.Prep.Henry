103d40194928dacb1e1c2981318dc2c5b09e392c
Jesus Alexis <ingjesusalexis@gmail.com>
2021-04-10 03:56:37 -0400

    Actualizado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -9,7 +9,13 @@ function deObjetoAmatriz(objeto){
       B: 2,
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
-  //Escribe tu código aquí
+  //Escribe tu código aquí:
+
+  // let array= [];
+  // for(let prop in objeto) {
+  //   array.push([prop,objeto[prop]])
+  // } return array;
+
  let arreglo =  Object.entries(objeto);
  return arreglo;
 }
